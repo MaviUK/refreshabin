fa9642e3ca563c619a34d268ed36a5364428e583
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-08-05 23:47:29 +0100

    Expose gift card purchase from restaurant storefronts

--- apps/restaurant/src/App.tsx
+++ apps/restaurant/src/App.tsx
@@ -24,6 +24,7 @@ import Marketing from './marketing/Marketing'
 import GiftCards from './gift-cards/GiftCards'
 import BuyGiftCard from './gift-cards/BuyGiftCard'
 import GiftCardSuccess from './gift-cards/GiftCardSuccess'
+import StorefrontGiftCardButton from './gift-cards/StorefrontGiftCardButton'
 import Storefront from './storefront/Storefront'
 import Checkout from './checkout/Checkout'
 import OrderStatus from './order/OrderStatus'
@@ -46,63 +47,52 @@ import './checkout/CheckoutAccount.css'
 import './checkout/CheckoutAddresses.css'
 
 export default function App() {
-  return (
-    <PlatformConfigurationProvider>
-      <PlatformStatusBoundary>
-        <SubscriptionAccessProvider>
-          <ApplicationRoutes />
-        </SubscriptionAccessProvider>
-      </PlatformStatusBoundary>
-    </PlatformConfigurationProvider>
-  )
+  return <PlatformConfigurationProvider><PlatformStatusBoundary><SubscriptionAccessProvider><ApplicationRoutes /></SubscriptionAccessProvider></PlatformStatusBoundary></PlatformConfigurationProvider>
 }
 
 function ApplicationRoutes() {
   const { configuration } = usePlatformConfiguration()
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/restaurants" element={<Restaurants />} />
-      <Route path="/r/:slug" element={<Storefront />} />
-      <Route path="/r/:slug/checkout" element={<Checkout />} />
-      <Route path="/r/:slug/gift-card" element={<BuyGiftCard />} />
-      <Route path="/r/:slug/gift-card/success" element={<GiftCardSuccess />} />
-      <Route path="/order/success" element={<OrderStatus />} />
-      <Route path="/account/login" element={<CustomerLogin />} />
-      <Route path="/account/register" element={<CustomerRegister />} />
-      <Route path="/account/forgot-password" element={<CustomerForgotPassword />} />
-      <Route path="/account/reset-password" element={<CustomerResetPassword />} />
-      <Route path="/account" element={<CustomerAccountHome />} />
-      <Route path="/account/profile" element={<CustomerProfile />} />
-      <Route path="/account/orders" element={<CustomerOrders />} />
-      <Route path="/account/addresses" element={<CustomerAddresses />} />
-      <Route path="/account/wallet" element={<CustomerWallet />} />
-      <Route path="/account/favourites" element={configuration.feature_flags.customer_favourites ? <CustomerFavourites /> : <Navigate to="/account" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-
-      <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-      <Route path="/orders" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
-      <Route path="/kds" element={<ProtectedRoute><KitchenDisplay /></ProtectedRoute>} />
-      <Route path="/printers" element={<ProtectedRoute><PrinterManagement /></ProtectedRoute>} />
-      <Route path="/print-history" element={<ProtectedRoute><PrintHistory /></ProtectedRoute>} />
-      <Route path="/onboarding" element={<ProtectedRoute allowApplication><Onboarding /></ProtectedRoute>} />
-      <Route path="/menu" element={<ProtectedRoute><MenuBuilder /></ProtectedRoute>} />
-      <Route path="/menu/modifiers" element={<ProtectedRoute><ModifierGroups /></ProtectedRoute>} />
-      <Route path="/opening-hours" element={<ProtectedRoute><OpeningHours /></ProtectedRoute>} />
-      <Route path="/branding" element={<ProtectedRoute><Branding /></ProtectedRoute>} />
-      <Route path="/settings" element={<ProtectedRoute><RestaurantSettings /></ProtectedRoute>} />
-      <Route path="/restaurant-details" element={<ProtectedRoute><RestaurantDetails /></ProtectedRoute>} />
-      <Route path="/delivery-areas" element={<ProtectedRoute><DeliveryAreas /></ProtectedRoute>} />
-      <Route path="/delivery-map" element={<ProtectedRoute><DeliveryMap /></ProtectedRoute>} />
-      <Route path="/payments" element={<ProtectedRoute allowPaymentSetup><Payments /></ProtectedRoute>} />
-      <Route path="/finance" element={<ProtectedRoute><Finance /></ProtectedRoute>} />
-      <Route path="/subscription" element={<ProtectedRoute allowPaymentSetup><Subscription /></ProtectedRoute>} />
-      <Route path="/marketing" element={<ProtectedRoute><Marketing /></ProtectedRoute>} />
-      <Route path="/gift-cards" element={<ProtectedRoute><GiftCards /></ProtectedRoute>} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  )
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/restaurants" element={<Restaurants />} />
+    <Route path="/r/:slug" element={<><Storefront /><StorefrontGiftCardButton /></>} />
+    <Route path="/r/:slug/checkout" element={<Checkout />} />
+    <Route path="/r/:slug/gift-card" element={<BuyGiftCard />} />
+    <Route path="/r/:slug/gift-card/success" element={<GiftCardSuccess />} />
+    <Route path="/order/success" element={<OrderStatus />} />
+    <Route path="/account/login" element={<CustomerLogin />} />
+    <Route path="/account/register" element={<CustomerRegister />} />
+    <Route path="/account/forgot-password" element={<CustomerForgotPassword />} />
+    <Route path="/account/reset-password" element={<CustomerResetPassword />} />
+    <Route path="/account" element={<CustomerAccountHome />} />
+    <Route path="/account/profile" element={<CustomerProfile />} />
+    <Route path="/account/orders" element={<CustomerOrders />} />
+    <Route path="/account/addresses" element={<CustomerAddresses />} />
+    <Route path="/account/wallet" element={<CustomerWallet />} />
+    <Route path="/account/favourites" element={configuration.feature_flags.customer_favourites ? <CustomerFavourites /> : <Navigate to="/account" replace />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/forgot-password" element={<ForgotPassword />} />
+    <Route path="/reset-password" element={<ResetPassword />} />
+    <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+    <Route path="/orders" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
+    <Route path="/kds" element={<ProtectedRoute><KitchenDisplay /></ProtectedRoute>} />
+    <Route path="/printers" element={<ProtectedRoute><PrinterManagement /></ProtectedRoute>} />
+    <Route path="/print-history" element={<ProtectedRoute><PrintHistory /></ProtectedRoute>} />
+    <Route path="/onboarding" element={<ProtectedRoute allowApplication><Onboarding /></ProtectedRoute>} />
+    <Route path="/menu" element={<ProtectedRoute><MenuBuilder /></ProtectedRoute>} />
+    <Route path="/menu/modifiers" element={<ProtectedRoute><ModifierGroups /></ProtectedRoute>} />
+    <Route path="/opening-hours" element={<ProtectedRoute><OpeningHours /></ProtectedRoute>} />
+    <Route path="/branding" element={<ProtectedRoute><Branding /></ProtectedRoute>} />
+    <Route path="/settings" element={<ProtectedRoute><RestaurantSettings /></ProtectedRoute>} />
+    <Route path="/restaurant-details" element={<ProtectedRoute><RestaurantDetails /></ProtectedRoute>} />
+    <Route path="/delivery-areas" element={<ProtectedRoute><DeliveryAreas /></ProtectedRoute>} />
+    <Route path="/delivery-map" element={<ProtectedRoute><DeliveryMap /></ProtectedRoute>} />
+    <Route path="/payments" element={<ProtectedRoute allowPaymentSetup><Payments /></ProtectedRoute>} />
+    <Route path="/finance" element={<ProtectedRoute><Finance /></ProtectedRoute>} />
+    <Route path="/subscription" element={<ProtectedRoute allowPaymentSetup><Subscription /></ProtectedRoute>} />
+    <Route path="/marketing" element={<ProtectedRoute><Marketing /></ProtectedRoute>} />
+    <Route path="/gift-cards" element={<ProtectedRoute><GiftCards /></ProtectedRoute>} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes>
 }
