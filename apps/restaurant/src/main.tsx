05907c75e0b89359d88c81dc62367da0ea700c08
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-08-09 11:51:42 +0100

    Load ordered.food global theme

--- apps/restaurant/src/main.tsx
+++ apps/restaurant/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './storefront/Customisation.css'
+import './theme.css'
 
 const rootElement = document.getElementById('root')
 
